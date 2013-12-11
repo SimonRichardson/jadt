6b61e696c0ac0a3e3033b9fd223e45ea8c134089
Simon Richardson <simon@ustwo.co.uk>
2013-12-11 21:56:11 +0000

    Adding .gitignore and adding one more test to option

--- src/test/java/jadt/OptionTest.java
+++ src/test/java/jadt/OptionTest.java
@@ -6,7 +6,18 @@ import org.junit.Test;
 public class OptionTest {
 
 	@Test
-	public void test() {
-		Assert.assertEquals("Option.of should equal Option.of with same value", Option.of(1), Option.of(1));
+	public void creation_with_Option_of() {
+		Assert.assertEquals("Option.of should equal Option.of with same value",
+				Option.of(1), Option.of(1));
+	}
+
+	@Test
+	public void map_and_increment() {
+		Assert.assertEquals("Option.of with map should be correct value",
+				Option.of(1).map(new Function1<Integer, Integer>() {
+					public Integer apply(Integer x) {
+						return x + 1;
+					}
+				}), Option.of(2));
 	}
 }
